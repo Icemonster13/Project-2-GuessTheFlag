8e353e5f4a1b8932b2a81bc8ed4fe046bcfe7de7
Michael Pascucci <mdpascucci@live.com>
2022-05-14 08:13:39 -0400

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,160 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Michael & Diana Pascucci on 4/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: - VIEWS
+    struct FlagImage: View {
+        var image: String
+        
+        var body: some View {
+            Image(image)
+                .renderingMode(.original)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .shadow(radius: 5)
+        }
+    }
+    
+    // MARK: - PROPERTIES
+    @State private var showingScore: Bool = false
+    @State private var gameOver: Bool = false
+    @State private var scoreTitle: String = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var userScore: Int = 0
+    @State private var questionCount: Int = 1
+    
+    // MARK: - ANIMATION PROPERTIES
+    @State private var degreesToSpin = 0.0
+    @State private var opacityAmount = 1.0
+    @State private var scaleAmount = 1.0
+    
+    // MARK: - FUNCTIONS
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            userScore += 1          // Increase the UserScore
+            degreesToSpin += 360
+            opacityAmount -= 0.75
+            scaleAmount -= 0.5
+        } else {
+            scoreTitle = "Wrong! That is the flag of\n \(countries[number])"
+        }
+        
+        if questionCount < 8 {
+            showingScore = true     // Trigger the ShowScore Alert
+            questionCount += 1      // Increment the QuestionCount
+        } else {
+            gameOver = true         // Trigger the GameOver Alert
+            questionCount += 1      // Increment the QuestionCount
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        opacityAmount = 1.0
+        scaleAmount = 1.0
+    }
+    
+    func resetGame() {
+        questionCount = 1       // Reset the QuestionCount to 1
+        userScore = 0           // Reset the UserScore to 0
+        askQuestion()
+    }
+    
+    // MARK: - BODY
+    var body: some View {
+        ZStack {
+            
+            //LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [Gradient.Stop(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                                   Gradient.Stop(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
+                           center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    } //: VStack
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            withAnimation {
+                                flagTapped(number)
+                            }
+                        } label: {
+                            FlagImage(image: countries[number])
+                        }
+                        .scaleEffect(number == correctAnswer ? 1 : scaleAmount)
+                        .rotation3DEffect(.degrees(number == correctAnswer ? degreesToSpin : 0), axis: (x: 0, y: 1, z: 0))
+                        .opacity(number == correctAnswer ? 1 : opacityAmount)
+                    } //: ForEach
+                } //: VStack
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(userScore)").titleStyle()
+                
+                Spacer()
+            }
+            .padding()
+        }
+        
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(userScore)")
+        }
+        
+        .alert("Game Over", isPresented: $gameOver) {
+            Button("Continue", action: resetGame)
+        } message: {
+            Text("You got \(userScore) of 8 flags correct!")
+        }
+    }
+}
+
+// MARK: - VIEW MODIFIERS
+struct MyTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.title.bold())
+    }
+}
+
+// MARK: - VIEW EXTENSIONS
+extension View {
+    func titleStyle() -> some View {
+        modifier(MyTitle())
+    }
+}
+
+// MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Michael & Diana Pascucci on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
